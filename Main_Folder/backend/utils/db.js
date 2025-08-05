0e9e8e1f6ef45f380e104112308074d777c79453
HritikKumar <hritikkr1724@gmail.com>
2025-08-05 23:50:07 +0530

    db error handling

--- Main_Folder/backend/utils/db.js
+++ Main_Folder/backend/utils/db.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         await mongoose.connect("mongodb+srv://hritikoff:icqAPh2oD6DgzsSP@cluster0.04rmqxg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-        console.log('mongodb connected successfully.');
+        console.log('mongodb connected 🌿');
     } catch (error) {
-        console.log(error);
+        console.error(`MongoDB Error: ${error.message}`);
     }
 }
 export default connectDB;
